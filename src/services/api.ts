8c3daf4ddd47b22c67fc35c931516f3708cd5607
Fabio Lopes <fabiolopes.dev@gmail.com>
2021-03-29 02:50:47 -0300

    Change API URL to secret

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/tractian/fake-api/',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
